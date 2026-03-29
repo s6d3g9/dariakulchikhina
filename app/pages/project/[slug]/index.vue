c3fb52083d3330847f696fb4fff27b857383b9dc
oxo <oxo@MacBook-Air-oxo.local>
2026-03-29 18:26:38 +0400

    chore(deploy): autosave before deploy 2026-03-29 14:26:38 UTC

---
+++ app/pages/project/[slug]/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div />
+</template>
+
+<script setup lang="ts">
+const route = useRoute()
+const slug = computed(() => String(route.params.slug || ''))
+
+if (!slug.value) {
+  await navigateTo('/login?role=client', { replace: true, redirectCode: 301 })
+} else {
+  await navigateTo(`/client/${encodeURIComponent(slug.value)}`, { replace: true, redirectCode: 301 })
+}
+</script>
---
+++ app/pages/project/login.vue
@@ -0,0 +1,11 @@
+<template>
+  <div />
+</template>
+
+<script setup lang="ts">
+definePageMeta({
+  layout: 'default',
+})
+
+await navigateTo('/login?role=client', { replace: true, redirectCode: 301 })
+</script>
